13fc1d533fd2bc881b4b00549b78484471496ab4
Omar Balach <din.email@example.com>
2025-09-20 18:07:00 +0200

    Lagt till SayHello-metod

---
+++ myapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Skriv ditt namn: ");
+        string name = Console.ReadLine();
+
+        // Validering för VG
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Du måste skriva in ett namn!");
+        }
+        else
+        {
+            SayHello(name);
+        }
+    }
+
+    static void SayHello(string name)
+    {
+        Console.WriteLine($"Hej {name}, välkommen till programmet!");
+    }
+}
